fc2d78dfa02115b77bc3553481b38537360d2c29
-po1yao <116749747+Chapalah336@users.noreply.github.com>
2025-02-15 15:57:10 +0300

    rebuild code

--- palindr.js
+++ palindr.js
@@ -1,16 +1,16 @@
-s = "adfghjkjhgfdarrrrrfffffvhnjbjhnjnghjmkdghjgdhj"
-function IskatelPolyndromusov(str) {
-    var polydeo = "";
-    for (var i = 0; i < str.length; i++) {
-        for (var u = i + 1; u <= str.length; u++) {
-            var subString = str.slice(i,u);
-            if (subString === subString.split('').reverse().join('') && subString.length > polydeo.length){
-               polydeo = subString;
-            }
-        }
+// s = "adfghjkjhgfdarrrrrfffffvhnjbjhnjnghjmkdghjgdhj"
+// function IskatelPolyndromusov(str) {
+//     var polydeo = "";
+//     for (var i = 0; i < str.length; i++) {
+//         for (var u = i + 1; u <= str.length; u++) {
+//             var subString = str.slice(i,u);
+//             if (subString === subString.split('').reverse().join('') && subString.length > polydeo.length){
+//                polydeo = subString;
+//             }
+//         }
 
-    }
-    return polydeo;
-}
-IskatelPolyndromusov(s);
-console.log(IskatelPolyndromusov(s));
+//     }
+//     return polydeo;
+// }
+// IskatelPolyndromusov(s);
+// console.log(IskatelPolyndromusov(s));
--- tournamentWinner.js
+++ tournamentWinner.js
@@ -1,34 +1,64 @@
+// competitions = [
+//     ["HTML", "C#"],
+//     ["C#", "Python"],
+//     ["Python","HTML"],
+// ]
+// results = [0, 0, 1]
+
+// function WhoIstournamentWinner(comp) {
+//     var hashMap = new Map();
+//     var tounrnWin = "";
+//     var winner ="";
+//     var currentPoints = 0
+//     for (var i = 0; i < comp.length; i++){
+//         var [homeTeam, awayTeam] = comp[i];
+//         if (results[i] === 0) {
+//             winner = awayTeam;
+//             hashMap.set(winner, (hashMap.get(winner) || 0) + 3);
+//         } 
+//         else {
+//             winner = homeTeam;
+//             hashMap.set(winner, (hashMap.get(winner) || 0) + 3);
+//         }
+//         console.log(hashMap)
+//        var Points = hashMap.get(winner);
+//        if (Points > currentPoints) {
+//         currentPoints = Points;
+//         tounrnWin = winner;
+//       }
+//     }
+//     return tounrnWin;
+// } 
+// WhoIstournamentWinner(competitions);
+// console.log(WhoIstournamentWinner(competitions));
+
 competitions = [
     ["HTML", "C#"],
     ["C#", "Python"],
     ["Python","HTML"],
 ]
 results = [0, 0, 1]
-
-function WhoIstournamentWinner(comp) {
-    var hashMap = new Map();
-    var tounrnWin = "";
-    var winner ="";
-    var currentPoints = 0
-    for (var i = 0; i < comp.length; i++){
+function WhoIstournamentWinner(comp, res) {
+    result = {
+        winnerteam : "",
+        currentPoints : 0
+    };
+    for (var i = 0; i < comp.length; i++) {
         var [homeTeam, awayTeam] = comp[i];
-        if (results[i] === 0) {
+        var winner;
+        if (res[i] === 0) {
             winner = awayTeam;
-            hashMap.set(winner, (hashMap.get(winner) || 0) + 3);
-        } 
-        else {
+            result[winner] = (result[winner] || 0) + 3;
+        } else {
             winner = homeTeam;
-            hashMap.set(winner, (hashMap.get(winner) || 0) + 3);
+            result[winner] = (result[winner] || 0) + 3;
         }
-        console.log(hashMap)
-       var Points = hashMap.get(winner);
-    //    if (Points > currentPoints) {
-    //     currentPoints = Points;
-    //     tounrnWin = winner;
-    //   }
+        if (result[winner] > result.currentPoints) {
+            result.currentPoints = result[winner];
+            result.winnerteam = winner;
+        }
+            console.log(result)
     }
-    return tounrnWin;
-} 
-WhoIstournamentWinner(competitions);
-console.log(WhoIstournamentWinner(competitions));
-
+    return result.winnerteam;
+}
+console.log(WhoIstournamentWinner(competitions, results));
